41d3d9d26729d3d847c4c361247ff0a559cfaef7
fundadormechaila2-cmyk <fundadormechaila2@gmail.com>
2025-08-22 15:36:46 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          mikay
+          mikay fundador
         </p>
         <a
           className="App-link" 
